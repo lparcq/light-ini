bb60fb1607cc43b5dc5b92e957b525806a8cfff6
Laurent Pelecq <lpelecq+rust@circoise.eu>
2020-06-20 21:49:56 +0200

    add readme, license and example

---
+++ README.md
@@ -0,0 +1,23 @@
+Light INI parser
+================
+
+This library implements an event-driven parser for the [INI file format](https://en.wikipedia.org/wiki/INI_file).
+
+```toml
+[dependencies]
+light_ini = "0.1"
+```
+
+See the documentation and the example in the code.
+
+## Format
+
+- There is no limitation in the names of the properties.
+
+- Comments are only allowed in their own line.
+
+- There is no escape or quoting characters
+
+## License
+
+Licensed under <a href="LICENSE-MIT">MIT license</a>.
---
+++ examples/hashmap.rs
@@ -0,0 +1,66 @@
+use light_ini::{IniHandler, IniParser};
+use std::collections::HashMap;
+use std::env;
+use std::path::PathBuf;
+
+type HandlerError = String;
+
+struct Handler {
+    pub globals: HashMap<String, String>,
+    pub sections: HashMap<String, HashMap<String, String>>,
+    section_name: Option<String>,
+}
+
+impl Handler {
+    fn new() -> Handler {
+        Handler {
+            globals: HashMap::new(),
+            sections: HashMap::new(),
+            section_name: None,
+        }
+    }
+}
+
+impl IniHandler for Handler {
+    type Error = HandlerError;
+
+    fn section(&mut self, name: &str) -> Result<(), Self::Error> {
+        self.section_name = Some(name.to_string());
+        match self.sections.insert(name.to_string(), HashMap::new()) {
+            Some(_) => Err(format!("{}: duplicate section", name)),
+            None => Ok(()),
+        }
+    }
+
+    fn option(&mut self, key: &str, value: &str) -> Result<(), Self::Error> {
+        match &self.section_name {
+            None => {
+                self.globals.insert(key.to_string(), value.to_string());
+                Ok(())
+            }
+            Some(ref section_name) => match self.sections.get_mut(section_name) {
+                Some(ref mut section) => {
+                    section.insert(key.to_string(), value.to_string());
+                    Ok(())
+                }
+                None => return Err(format!("{}: unknown section", section_name)),
+            },
+        }
+    }
+
+    fn comment(&mut self, _: &str) -> Result<(), Self::Error> {
+        Ok(())
+    }
+}
+
+fn main() {
+    for filename in env::args().skip(1) {
+        let mut handler = Handler::new();
+        let mut parser = IniParser::new(&mut handler);
+        let path = PathBuf::from(&filename);
+        parser.parse_file(path).unwrap();
+        println!("File {}", filename);
+        println!("Globals {:#?}", handler.globals);
+        println!("Sections {:#?}", handler.sections);
+    }
+}
